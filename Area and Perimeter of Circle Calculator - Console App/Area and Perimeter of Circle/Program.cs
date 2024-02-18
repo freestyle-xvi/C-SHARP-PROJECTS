feeea488f56d22dcfa3adc26324febff9213eb0a
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:35:15 +0200

    Add files via upload

---
+++ Area and Perimeter of Circle Calculator - Console App/Area and Perimeter of Circle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Area_and_Perimeter_of_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double perimeter;
+            double area;
+
+            Console.WriteLine("Area and Perimeter of Circle Calculator");
+
+            Console.WriteLine("\nEnter the radius of your circle");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            perimeter = 2 * 3.14 * radius;
+            area = 3.14 * radius * radius;
+
+            Console.WriteLine("\nThe perimeter of your circle is :  " + perimeter);
+            Console.WriteLine("The area of your circle is :  " +  area);
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
