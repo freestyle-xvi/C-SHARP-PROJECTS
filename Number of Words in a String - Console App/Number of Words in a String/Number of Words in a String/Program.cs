105c6689a736c2212da449b661adf575cc601358
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:43:13 +0200

    Add files via upload

---
+++ Number of Words in a String - Console App/Number of Words in a String/Number of Words in a String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Number_of_Words_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sentence;
+            Console.WriteLine("Enter a string : ");
+            sentence = Console.ReadLine();
+            String[] words = sentence.Split(' ');
+            Console.WriteLine("Number of words in the sentence : " + words.Length);
+            Console.ReadKey();
+        }
+    }
+}
