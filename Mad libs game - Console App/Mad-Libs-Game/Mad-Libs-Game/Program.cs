cf5365e4e0db921c08547afc6e496467d1618e7a
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:41:28 +0200

    Add files via upload

---
+++ Mad libs game - Console App/Mad-Libs-Game/Mad-Libs-Game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mad_Libs_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MAD LIBS GAME ........... idk what that even is ngl but i understand the concept lmfao.
+
+            String color, pluralNoun, celebrity;
+
+            Console.WriteLine("| MAD LIBS GAME |");
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity or fictional character: ");
+            celebrity = Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine("\nRoses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I hate " + celebrity);
+
+            Console.ReadKey();
+        }
+    }
+}
+
