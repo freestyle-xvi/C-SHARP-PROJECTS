384be05667f7f55c637067e2e8457253eab574e7
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:40:14 +0200

    Add files via upload

---
+++ Even or Odd - Console App/Even or Odd/Even or Odd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number : ");
+            num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("--------------------");
+            if(num%2==0)
+            {
+                Console.WriteLine(num + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine( num + " is an odd number");
+            }
+            Console.WriteLine("--------------------");
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
