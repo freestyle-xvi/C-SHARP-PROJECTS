3b9e81e005ae2ca9064d2d8d78c660b38caafc07
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:37:02 +0200

    Add files via upload

---
+++ Average of 3 numbers - Console App/Average of 3 numbers/Average of 3 numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Average_of_3_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            int average;
+
+            Console.WriteLine("Average of 3 numbers program");
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("\nEnter the 1st number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd number: ");
+            num2 =  Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 3rd number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            average = (num1 + num2 + num3)  / 3;
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("The average of the 3 numbers is : " + average);
+            Console.WriteLine("------------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
